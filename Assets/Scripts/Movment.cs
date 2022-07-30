acdd66db64aee09abadbbb7a4a4fd33667baf735
Elinor Zusin <elinorzusin@Elinors-Mini-2.lan>
2022-07-30 16:33:19 +0300

    Shooting game

---
+++ Assets/Scripts/Movment.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movment : MonoBehaviour
+{
+    public float speed = 12f;
+
+    public CharacterController controller;
+
+    public float gravity = -9.81f;
+
+    Vector3 velocity;
+    // Update is called once per frame
+    void Update()
+    {
+        moving();
+    }
+
+
+    public void moving()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * speed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+
+
+    }
+
+}
